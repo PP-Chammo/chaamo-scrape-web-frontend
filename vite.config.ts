121aa890e4409bd39c6d21c8baaa06efd7c47f2f
PP-MDNR <pp-mdnr@PP-MDNRs-Mac-mini.local>
2025-06-25 07:19:59 +0100

    config gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { ghPages } from "vite-plugin-gh-pages";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  base: "/chaamo-scrape-web-frontend/",
+  plugins: [react(), tailwindcss(), ghPages({ branch: "master" })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
