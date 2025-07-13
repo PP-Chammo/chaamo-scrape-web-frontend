b5b6b2dc2401252abf15fb772e48bb67d0902121
PP-Chammo <pasquale.palena.chammo@gmail.com>
2025-07-13 23:41:23 +0100

    fix: build deployment 3

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/chaamo-scrape-web-frontend/', // github pages pathname
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
